8dc31b63de8b08b0d15a47aa555176c3803a2b01
fil <fil@localhost.localdomain>
2018-03-19 16:50:24 +0100

    Initial import

---
+++ writefifo.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+int fd;
+char plcfifo[32];
+char	readlin[32];
+    sprintf(plcfifo,"/tmp/plcfifo");
+
+    /* create the FIFO (named pipe) */
+    mkfifo(plcfifo, 0666);
+    printf("argc = %d\n",argc);
+    printf("argv = %s\n",argv[argc-1]);
+
+    if ( argc != 2)
+    {
+	    printf("argc = %d\n",argc);
+	    exit(1);
+    }
+
+    bzero(readlin,32);
+    strcat(readlin,argv[argc-1]);
+
+
+    fd = open(plcfifo, O_WRONLY);
+    write(fd, readlin, strlen(readlin));
+    close(fd);
+
+    /* remove the FIFO */
+    unlink(plcfifo);
+
+    exit(0);
+}
